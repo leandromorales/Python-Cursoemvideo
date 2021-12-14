786c82f5ae0bed893e98d0dc8816f52536193199
leandro_morales <leandro.cmorales@gmail.com>
2021-12-14 15:26:01 -0300

    Prática dia 14/12 - Exercício 32

--- Curso_em_video_Mundo_1/exercícios 28 - 35/ex032.py
+++ Curso_em_video_Mundo_1/exercícios 28 - 35/ex032.py
@@ -1,1 +1,14 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é Bissexto.
+
+ano = int(input('Digite um ano: '))
+bis = ano % 4
+biss = ano % 100
+bisse = ano % 400
+
+if bis == 0:
+    if biss == 0:
+        if bisse == 0:
+            print('O ano {} é bissexto')
+print(bis)
+print(biss)
+print(bisse)
