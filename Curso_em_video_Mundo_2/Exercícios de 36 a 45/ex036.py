c60a54f9318c7f278385d0b3a397b1903d4d2944
Leandro_morales <leandro.cmorales@gmail.com>
2021-12-18 23:03:43 -0300

    Prática dia 18/12/2021 - Exercício 36

--- Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex036.py
+++ Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex036.py
@@ -2,3 +2,18 @@
 #O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 #Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será
 #negado.
+
+
+valor = float(input('Digite o valor da casa que você quer comprar: R$'))
+salario = float(input('Digite o seu salário atual: R$'))
+anos = float(input('Digite em quantos anos você que pagar o financiamento: '))
+
+presta = valor / (anos*12)
+porcentagem = (salario * 30) / 100
+
+if presta > porcentagem:
+    print('''O financiamento não foi aprovado. 
+    A prestação no valor de "R${:.2f}" excede o limite de 30% do salário.'''.format(presta))
+else:
+    print('O financiamento foi aprovado e a prestação será no valor de "R${:.2f}"'.format(presta))
+
