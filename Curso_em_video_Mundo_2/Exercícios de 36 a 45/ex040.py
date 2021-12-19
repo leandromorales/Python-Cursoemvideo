c4ecfa72090604af48332d147222067ff0d00ca0
Leandro_morales <leandro.cmorales@gmail.com>
2021-12-19 18:43:27 -0300

    Prática dia 19/12/2021 - Exercício 40.

--- Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex040.py
+++ Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex040.py
@@ -3,3 +3,14 @@
 # - Média abaixo de 5.0: Reprovado;
 # - Média entre 5.0 e 6.9: Recuperação;
 # - Média 7.0 ou superior: Aprovado.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+
+if media < 5.0:
+    print('O aluno foi reprovado com média {:.1f}.'.format(media))
+elif media >= 5.0 and media <= 6.9:
+    print('O aluno está em recuperação com média {:.1f}'.format(media))
+else:
+    print('O aluno foi aprovado com média {:.1f}'.format(media))
