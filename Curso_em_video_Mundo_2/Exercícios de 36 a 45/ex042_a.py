76386a6def97343dfc12b26eece27db5b383e246
Leandro_morales <leandro.cmorales@gmail.com>
2021-12-20 22:32:36 -0300

    Prática dia 20/12/2021 - Exercícios 42 e 43.

--- Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex042_a.py
+++ Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex042_a.py
@@ -2,18 +2,17 @@
 # - Equilátero: todos os lados iguais;
 # - Isósceles: dois lados iguais;
 # - Escaleno: todos os lados diferentes.
-
 l1 = float(input('Digite o comprimento da primeira reta: '))
 l2 = float(input('Digite o comprimento da segunda reta: '))
 l3 = float(input('Digite o comprimento da terceira reta: '))
 
 if l1 < l2 + l3 and l2 < l1 + l3 and l3 < l1 + l2:
-    if l1 == l2 and l1 == l3:
-        print('Estas retas formam um triângulo equilátero.')
-    elif l1 == l2 or l1 == l3 or l2 == l3:
-        print('Estas retas formam um triângulo isósceles.')
+    if l1 == l2 == l3:
+        print('As retas formam um triângulo equilátero.')
+    elif l1 != l2 != l3 != l1:
+        print('As retas formam um triângulo escaleno.')
     else:
-        print('Estas retas formam um triângulo escaleno.')
+        print('As retas formam um triângulo isósceles.')
 
 else:
-    print('As três retas não formam um triângulo.')
+    print('As retas não formam um triângulo.')
--- Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex043.py
+++ Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex043.py
@@ -5,3 +5,21 @@
 # - Entre 25 até 30: Sobrepeso;
 # - Entre 30 até 40: Obesidade;
 # - Acima de 40: Obesidade mórbida.
+
+peso = float(input('Digite o seu peso em KG: '))
+altura = float(input('Digite a sua altura: '))
+imc = peso / altura ** 2
+#print(imc)
+
+m1 = 'O seu IMC é de'
+m2 = 'e você está com'
+if imc < 18.5:
+    print('{} {:.1f} e você está abaixo do peso.'.format(m1, imc))
+elif imc <= 25:
+    print('{} {:.1f} {} peso normal.'.format(m1, imc, m2))
+elif imc <= 30:
+    print('{} {:.1f} {} sobrepeso.'.format(m1, imc, m2))
+elif imc <= 40:
+    print('{} {:.1f} {} obesidade.'.format(m1, imc, m2))
+else:
+    print('{} {:.1f} {} obesidade mórbida'.format(m1, imc, m2))
