7476d9dffac5bac8bac8fd98dd9428ae514c8b01
leandro_morales <leandro.cmorales@gmail.com>
2021-12-21 13:48:49 -0300

    21/12/2021 - Prática do dia - ex044.py

--- Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex044.py
+++ Curso_em_video_Mundo_2/Exercícios de 36 a 45/ex044.py
@@ -3,3 +3,44 @@
 # - À vista no cartão: 5% de desconto;
 # - Em até 2x no cartão: preço normal;
 # - 3x ou mais no cartão: 20% de juros.
+
+def calculo():
+    #print(condicao)
+    if condicao == 1:
+        desconto = (preco * 10)/100
+        final = preco - desconto
+        print('Opção à vista Dinheiro/Cheque escolhida.')
+        print('Valor final da compra: R${}'.format(final))
+    elif condicao == 2:
+        desconto = (preco * 5)/100
+        final = preco - desconto
+        print('Opção à vista no Cartão escolhida.')
+        print('Valor final da compra: R${}'.format(final))
+    elif condicao == 3:
+        print('Você escolheu a opção normal de ')
+    else:
+        print('teste')
+preco = float(input('Digite o valor do produto: R$ '))
+
+contador = 4
+condicao = 2
+while contador > 0:
+    condicao = int(input('''Digite o número correspondente à condição de pagamento:
+    [ 1 ] 10% de desconto à vista no Dinheiro/Cheque;
+    [ 2 ] 5% de desconto à vista no cartão;
+    [ 3 ] 2x no cartão;
+    [ 4 ] 3x com juros de 20%.
+    Sua opção: '''))
+    if condicao >= 1 and condicao <= 4:
+        calculo()
+        contador = 0
+    else:
+        contador = contador - 1
+        if contador == 1:
+            print('Opção incorreta. Você tem mais uma tentativa.')
+        elif contador == 0:
+            print('0 programa será encerrado.')
+        else:
+            print('Opção incorreta. Você tem {} tentativas.'.format(contador))
+
+#print('Condição')
