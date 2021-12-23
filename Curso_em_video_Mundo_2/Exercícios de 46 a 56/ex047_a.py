683bc8ba72739a773f4d234a402fb86f9b56b94c
Leandro_morales <leandro.cmorales@gmail.com>
2021-12-22 22:49:49 -0300

    Prática dia 22/12/2021 - Exercícios 46 - 48

--- Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex047_a.py
+++ Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex047_a.py
@@ -1,6 +1,5 @@
 #Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 a 50.
 
-for c in range(1, 51):
-    if c % 2 == 0:
-        print(c, end=' ')
-
+for c in range(2, 51, 2):
+    print(c, end=' ')
+print('Acabou.')
--- Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex048.py
+++ Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex048.py
@@ -1,1 +1,12 @@
-#Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+#Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de
+# três e que se encontram no intervalo de 1 até 500.
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma += c
+        cont = cont + 1
+
+print('A soma dos {} valores solicitados é igual a: {}.'.format(cont, soma))
+#print(soma)
+#print(cont)
