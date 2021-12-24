f7680a6f7e230ca6138b7f53e305522f9f02ca5f
leandro_morales <leandro.cmorales@gmail.com>
2021-12-24 14:26:36 -0300

    24/12/2021 - 1ª Tentativa ex053.py

--- Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex053.py
+++ Curso_em_video_Mundo_2/Exercícios de 46 a 56/ex053.py
@@ -4,3 +4,32 @@
 #a torre da derrota
 #o lobo ama bolo
 #Anotaram a data da maratona
+
+frase = str(input('Digite uma frase: ')).strip()
+
+#print(frase)
+
+#remover os espaços:
+
+frase2 = frase.replace(' ', '')
+frase3 = frase2
+
+#print(frase2)
+
+#comparar caracteres
+
+for c in range(0, len(frase2)):
+    print(frase2[c], end='-')
+    print(c, end=' ')
+
+print('\n')
+for d in range(len(frase2) - 1, -1, -1):
+    print(frase2[d], end='-')
+    print(d, end=' ')
+
+
+#for c in range(0, len(frase)):
+#    #print(c)
+#    if frase[c] == ' ':
+#        frase2 = frase.replace(' ', '')
+
