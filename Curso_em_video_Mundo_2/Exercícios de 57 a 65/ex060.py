f9f67dd5aba359af0dd8dee1eb6896c4c31394de
leandro_morales <leandro.cmorales@gmail.com>
2021-12-29 14:03:12 -0300

    29/12/2021 - Tentativa Ex060.

--- Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex060.py
+++ Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex060.py
@@ -1,2 +1,8 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Ex: 5! = 5x4x3x2x1 = 120
+
+num = int(input('Digite um número: '))
+while num > 0:
+    fat = num * (num - 1)
+    num -= 1
+print('O Fatorial de {} é igual a {}.'.format(num, fat))
