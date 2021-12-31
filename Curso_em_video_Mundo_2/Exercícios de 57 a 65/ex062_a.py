5f8b060e634599dd856e36cabdc5a523e755fc6e
leandro_morales <leandro.cmorales@gmail.com>
2021-12-31 13:53:52 -0300

    30/12/2021 - Finalização do Ex062.

--- Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex062_a.py
+++ Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex062_a.py
@@ -5,18 +5,21 @@ from time import sleep
 
 termo = int(input('Digite o primeiro termo da PA: '))
 razao = int(input('Digite a razão da PA: '))
-cont = 1
+cont = 0
 end = 1
 final = int(input('''Digite a quantidade de termos que você quer visualizar ou 
 Digite 0 para encerrar o programa: '''))
-while cont <= final and end != 1:
+while cont <= final and end != 0:
     if final == 0:
         print('O programa será encerrado.')
         sleep(2)
         break
     if cont == final:
-        print('{} - Fim.'.format(termo))
-        #end = int(input('teste'))
+        end = int(input('\nDigite [0] para encerrar o programa ou digite quantos valores deseja exibir a mais?: '))
+        if end == 0:
+            print('O programa será encerrado.')
+        else:
+            final = end + 1
     else:
         print('{} -> '.format(termo), end='')
     termo += razao
