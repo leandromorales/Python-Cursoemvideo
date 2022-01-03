66cb71723e256e0e76cedeb2918375c3af1f09f5
leandro_morales <leandro.cmorales@gmail.com>
2022-01-03 13:29:07 -0300

    03/01/2021 - Finalização do Ex063.

--- Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex063.py
+++ Curso_em_video_Mundo_2/Exercícios de 57 a 65/ex063.py
@@ -1,3 +1,16 @@
 # Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma
 # Sequência de Fibonacci.
 # Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
+seq = int(input('Digite a quantidade de elementos da sequencia: ')) + 1
+cont = 1
+t1 = 0
+t2 = 1
+print('{} -> {} -> '.format(t1, t2), end='')
+while cont != seq:
+    t3 = t1 + t2
+    print('{} -> '.format(t3), end='')
+    cont += 1
+    t1 = t2
+    t2 = t3
+print(' FIM')
