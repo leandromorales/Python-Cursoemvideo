8801febaa7e5f1911fd9777bd73e30ab1028657f
leandro_morales <leandro.cmorales@gmail.com>
2022-01-05 15:01:21 -0300

    05/01/2021 - Ex066 e ex067.

--- Curso_em_video_Mundo_2/Exercícios de 66 a 71/ex067_a.py
+++ Curso_em_video_Mundo_2/Exercícios de 66 a 71/ex067_a.py
@@ -2,10 +2,11 @@
 # digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 
 while True:
-    num = int(input('Digite um número para ver a sua tabuada: '))
-    if num < 0:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
         break
+    print('-' * 30)
     for c in range(1, 11):
-        tab = c * num
-        print(f'{c} X {num}  = {tab}')
-print('Fim')
+        print(f'{n} X {c} = {n * c}')
+    print('-' * 30)
+print('Programa de tabuada encerrado: ')
