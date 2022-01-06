840e56929fdf793294e47f16d86130c366ecbf0f
Leandro_morales <leandro.cmorales@gmail.com>
2022-01-06 07:14:46 -0300

    Prática dia 06/01/2022 - Ex066, ex067 e ex068.

--- Curso_em_video_Mundo_2/Exercícios de 66 a 71/ex067.py
+++ Curso_em_video_Mundo_2/Exercícios de 66 a 71/ex067.py
@@ -1,2 +1,11 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor
 # digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    num = int(input('Digite um número para ver a sua tabuada: '))
+    if num < 0:
+        break
+    for c in range(1, 11):
+        tab = c * num
+        print(f'{c} X {num}  = {tab}')
+print('Fim')
