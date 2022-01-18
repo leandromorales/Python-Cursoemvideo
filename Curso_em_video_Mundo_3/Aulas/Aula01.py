da58fc2478f4f841a30c5bf2c4f98c8deecb75a2
leandro_morales <leandro.cmorales@gmail.com>
2022-01-18 14:19:48 -0300

    18/01/2021 - Continuação aula 1.

--- Curso_em_video_Mundo_3/Aulas/Aula01.py
+++ Curso_em_video_Mundo_3/Aulas/Aula01.py
@@ -1,4 +1,25 @@
 lanche = 'Hamburguer', 'Suco', 'Pizza', 'Pudim'
 print(lanche)
-
+print(lanche[1])
+print(lanche[1:])
+print(lanche[-2])
+print(lanche[-3:])
 print(len(lanche))
+
+
+
+for cont in lanche:
+    print(f'Eu vou comer {cont}')
+print('Comi para caramba.')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+print('Comi para caramba.')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+
+
+#As Tuplas são imutáveis durante a execução do programa.
+
