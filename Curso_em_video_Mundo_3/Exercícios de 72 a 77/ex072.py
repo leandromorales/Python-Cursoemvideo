e188d4db137e1b0b3d6387aba9d6f47ae4e88081
leandro_morales <leandro.cmorales@gmail.com>
2022-01-19 13:30:08 -0300

    19/01/2021 - Tentativa exercício 72.

--- Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex072.py
+++ Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex072.py
@@ -1,2 +1,13 @@
 #Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero
 #até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+
+nums = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze',
+        'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
+ent = int(input('Digite um número entre 0 e 20: '))
+
+while ent not in range(0, 21):
+    ent = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {nums[ent]}')
+print('Fim')
---
+++ Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex072_a.py
@@ -0,0 +1,19 @@
+#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero
+#até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze',
+        'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    while True:
+        num = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= num <= 20:
+            print('break')
+            break
+
+        print('Tente novamente. ', end='')
+    print(f'Você digitou o número {cont[num]}')
+    while True:
+        tent = str(input('Deseja continuar? [S/N]')).strip().upper()
+        if tent == 'N':
+            print('O programa será encerrado.')
+            break
