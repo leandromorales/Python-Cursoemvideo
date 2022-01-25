b07037f3a9a3b8bf5e8fa01afdd9176825aff2b6
leandro_morales <leandro.cmorales@gmail.com>
2022-01-24 21:52:21 -0300

    24/01/2021 - Exercício 76.

--- Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex076.py
+++ Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex076.py
@@ -3,3 +3,16 @@ Crie um programa que tenha uma tupla única com nomes de produtos e seus respect
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
 '''
+
+produtos = ('Lápis', 2.50, 'Caneta', 3.00, 'Lapiseira', 4.50, 'Caderno', 22.00, 'Borracha', 0.75)
+
+#print(produtos)
+print('-' * 40)
+print(f'{"Lista de preços":^40}')
+print('-' * 40)
+for n in range(0, len(produtos)):
+    if n % 2 == 0:
+        print(f'{produtos[n]:.<30}', end=' ')
+    else:
+        print(f'R${produtos[n]:>6.2f}')
+print('-' * 40)
--- Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex077.py
+++ Curso_em_video_Mundo_3/Exercícios de 72 a 77/ex077.py
@@ -2,3 +2,4 @@
 Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 '''
+
