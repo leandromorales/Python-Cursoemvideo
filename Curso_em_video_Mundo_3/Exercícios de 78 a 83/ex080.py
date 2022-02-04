d3039d3b16d39fb53bcb2d4f5d54f786faa142ee
Leandro_morales <leandro.cmorales@gmail.com>
2022-02-03 21:52:18 -0300

    Dia 03/02/2022 - Finalização do exercício 80.

--- Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex080.py
+++ Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex080.py
@@ -2,27 +2,39 @@
 uma lista, já na posição correta de inserção (sem usar o sort()).
 No final mostre a lista ordenada na tela.'''
 
-#valores = [0, 0, 0, 0, 0]
+
 valores = list()
 
-#Receber 5 valores pelo teclado
 for c in range(0, 5):
     num = int(input('Digite um número: '))
-    #Jogar o primeiro valor na lista:
     if len(valores) == 0:
         valores.append(num)
-        print('Valor adicionado ao final da lista!')
-    #Verificar se o número é maior do que os que já existem na lista
+        print('NNúmero adicionado ao final da lista.')
+    elif num in valores:
+        print('Este número já foi digitado.')
     else:
-        if num in valores:
-            print(f'O número "{num}" já foi digitado nesta sequência')
-        else:
-            if num > max(valores):
-                valores.append(num)
-                print('Valor adicionado ao final da lista!')
-            if num < min(valores):
-                posi = valores.index(min(valores))
-                valores.insert(posi - 1, num)
-                posii = posi - 1
-                print(f'Valor adicionado na posição {posii} da lista!')
+        if num > max(valores):
+            valores.append(num)
+            print('Número adicionado ao final da lista.')
+        if num < min(valores):
+            pos = valores.index(min(valores))
+            print(f'Valor adicionado na posição {pos}.')
+            valores.insert(pos, num)
+        if len(valores) >= 2:
+            if num > valores[0] and num < valores[1]:
+                print(f'Valor adicionado na posição {1}.')
+                valores.insert(1, num)
+        if len(valores) >= 3:
+            if num > valores[1] and num < valores[2]:
+                print(f'Valor adicionado na posição {2}.')
+                valores.insert(2, num)
+        if len(valores) >= 4:
+            if num > valores[2] and num < valores[3]:
+                print(f'Valor adicionado na posição {3}.')
+                valores.insert(3, num)
+        if len(valores) >= 5:
+            if num > valores[3] and num < valores[4]:
+                print(f'Valor adicionado na posição {4}.')
+                valores.insert(4, num)
+
 print(valores)
