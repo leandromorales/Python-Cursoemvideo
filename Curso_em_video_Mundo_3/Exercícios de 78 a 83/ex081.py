2271794f925a0165597e239f6e32fa60704406c7
Leandro_morales <leandro.cmorales@gmail.com>
2022-02-06 19:48:26 -0300

    Dia 06/02/2022 - Exercício 81.

--- Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex081.py
+++ Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex081.py
@@ -11,17 +11,16 @@ while True:
     while not num.isnumeric():
         num = input('Digite um número: ')
     lista.append(int(num))
-    cont = str(input('Deseja continuar? [S/N]')).upper().strip()
     num = ''
-    while cont not in 'SN':
-        print('while')
-        cont = str(input('Deseja continuar? [S/N]')).upper().strip()
-        if cont == 'N':
-            print('Break')
-            break
-            break
-    print('Fora')
-    cont = ''
+    cont = str(input('Deseja continuar? [S/N]')).strip().upper()
+    if cont == 'N':
+        break
 
 print(f'Foram digitados {len(lista)} números.')
+lista.sort(reverse= True)
 print(lista)
+num = lista.count(5)
+if num == 0:
+    print('O número 5 não foi digitado.')
+else:
+    print(f'O número 5 foi digitado {num} vezes.')
