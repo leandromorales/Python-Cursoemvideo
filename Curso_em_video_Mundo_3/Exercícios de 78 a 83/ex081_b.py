989fa3fde5cb6ab24f01323a4be0fce601475246
Leandro_morales <leandro.cmorales@gmail.com>
2022-02-07 23:20:52 -0300

    Dia 07/02/2022 - Exercício 82.

---
+++ Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex081_b.py
@@ -0,0 +1,29 @@
+'''Crie um programa que vai ler vários números e colocar em uma lista.
+Depois disso, mostre:
+
+A) Quantos números foram digitados;
+B) A lista de valores ordenada de forma decrescente;
+C) Se o valor 5 foi digitado e está ou não na lista.'''
+
+lista = []
+num = ''
+while True:
+    while not num.isnumeric():
+        num = input('Digite um número: ')
+    lista.append(int(num))
+    num = ''
+    cont = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    while cont != 'N' and cont != 'S':
+        #print(f'cont {cont}')
+        cont = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    if cont in 'N':
+        break
+
+print(f'Foram digitados {len(lista)} números.')
+lista.sort(reverse= True)
+print(lista)
+num = lista.count(5)
+if num == 0:
+    print('O número 5 não foi digitado.')
+else:
+    print(f'O número 5 foi digitado {num} vezes.')
--- Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex082.py
+++ Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex082.py
@@ -2,3 +2,26 @@
 Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores
 ímpares digitados, respectivamente.
 Ao final, mostre o resultado das três listas geradas.'''
+
+valores = []
+par = []
+imp = []
+num = ''
+while True:
+    while not num.isnumeric():
+        num = input('Digite um número: ')
+    numm = int(num)
+    valores.append(numm)
+    if numm % 2 == 0:
+        par.append(numm)
+    else:
+        imp.append(numm)
+    num = ''
+    cont = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    while cont != 'S' and cont != 'N':
+        cont = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    if cont == 'N':
+        break
+print(f'Os números digitados foram: {valores}')
+print(f'Entre os números digitados, estes são pares {par},')
+print(f'e estes são ímpares {imp}')
