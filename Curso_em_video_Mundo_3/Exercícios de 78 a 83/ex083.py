43f4691ec1fb698c3dfe1b3832254aac83277c24
Leandro_morales <leandro.cmorales@gmail.com>
2022-02-09 22:04:01 -0300

    Dia 09/02/2022 - Exercício 83.

--- Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex083.py
+++ Curso_em_video_Mundo_3/Exercícios de 78 a 83/ex083.py
@@ -1,3 +1,11 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos
 e fechados na ordem correta.'''
+
+
+valores = str(input('Digite uma expressão: '))
+#print(valores)
+if valores.count('(') == valores.count(')'):
+    print('A expressão digitada é válida.')
+else:
+    print('A expressão digitada é inválida.')
