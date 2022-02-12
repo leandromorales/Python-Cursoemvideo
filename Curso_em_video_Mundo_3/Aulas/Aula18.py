d13865e7c4f1059a811ac9db932841767dbb30fc
Leandro_morales <leandro.cmorales@gmail.com>
2022-02-11 23:01:23 -0300

    Dia 11/02/2022 - Aula 18.

---
+++ Curso_em_video_Mundo_3/Aulas/Aula18.py
@@ -0,0 +1,38 @@
+
+
+#Duas listas com ligação entre si
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste)
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste)
+print(galera)
+
+#Duas listas independentes
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
+
+#Lista composta
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+
+print(galera[0])
+print(galera[2] [1])
+
+galera = list()
+dado = list()
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+print(galera)
