4f34f5d1d562e1d6a78c4ae2000f9892f18bd6a2
Leandro_morales <leandro.cmorales@gmail.com>
2022-03-15 22:01:10 -0300

    Dia 15/03/2022 - Ex090.

--- Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex090.py
+++ Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex090.py
@@ -1,2 +1,15 @@
 '''Faça um programa que leia nome e média de um aluno, guardando também a situação em um
 dicionário. No final, mostre o conteúdo da estrutura na tela.'''
+
+boletim = dict()
+boletim['Nome'] = str(input('Nome: '))
+boletim['Média'] = float(input('Média: '))
+if boletim['Média'] >= 7:
+    boletim['Situação'] = 'Aprovado'
+else:
+    boletim['Situação'] = 'Reprovado'
+
+for k, v in boletim.items():
+    print(f'- {k} é igual a {v}')
+
+#print(boletim)
