af9b7b6fa23d62afd37c77d571d49c898a7d7cc4
Leandro Morales <leandro.cmorales@gmail.com>
2022-03-28 22:52:44 -0300

    28/03/2022 - Exercício 092.

--- Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex092.py
+++ Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex092.py
@@ -2,5 +2,24 @@
 (com idade) em um dicionário. Se por acaso a CTPS for diferente de zero, o dicionário receberá
 também o ano da contratação e o salário. Calcule e acrescente, além da idade, com quantos anos
 a pessoa vai se aposentar.'''
-
 #35 anos de contribuição para se aposentar.
+from datetime import date
+#ano = date.today().year
+ano = 2017
+trab = dict()
+trab['nome'] = str(input('Nome: '))
+nascimento = int(input('Ano de nascimento: '))
+trab['idade'] = ano - nascimento
+trab['ctps'] = int(input('Carteira de trabalho (0 não tem): '))
+if trab['ctps'] != 0:
+    trab['contratação'] = int(input('Ano de Contratação: '))
+    trab['salário'] = float(input('Salário: '))
+    trab['aposentadoria'] = (trab['contratação'] - nascimento) + 35
+
+print('-=' * 30)
+for x, y in trab.items():
+    print(f'- {x} tem o valor {y}')
+
+
+
+
