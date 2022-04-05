df97cb5b4af06d3001e87afee5f99aeb3b12bd39
Leandro Morales <leandro.cmorales@gmail.com>
2022-04-04 22:13:59 -0300

    04/04/2022 - Exercício 093.

--- Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex093.py
+++ Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex093.py
@@ -2,3 +2,26 @@
 vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols
 feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluíndo o total
 de gols feitos durante o campeonato.'''
+
+apv = dict()
+gols = list()
+total = 0
+apv['Nome'] = str(input('Nome do Jogador: '))
+partidas = int(input(f'Quantas partidas {apv["Nome"]} jogou: '))
+
+for i in range(0, partidas):
+    gol = int(input(f'Quantos gols na partida {i}? '))
+    total += gol
+    gols.append(gol)
+apv['Gols'] = gols[:]
+apv['Total'] = total
+
+print('-=' * 30)
+print(apv)
+print('-=' * 30)
+for x, y in apv.items():
+    print(f'O campo {x} tem o valor {y}')
+print('-=' * 30)
+print(f'O jogador {apv["Nome"]} jogou {partidas} partidas.')
+for x, y in enumerate(apv['Gols']):
+    print(f'=> Na partida {x} fez {y} gols.')
