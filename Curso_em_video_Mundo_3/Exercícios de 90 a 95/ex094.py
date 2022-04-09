61b5170b865dc941cff4f9a49bb579c1d75b9b83
Leandro Morales <leandro.cmorales@gmail.com>
2022-04-08 23:27:26 -0300

    08/04/2022 - Exercício 094.

--- Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex094.py
+++ Curso_em_video_Mundo_3/Exercícios de 90 a 95/ex094.py
@@ -4,3 +4,50 @@ A) Quantas pessoas foram cadastradas;
 B) A média de idade do grupo;
 C) Uma lista com todas as mulheres;
 D) Uma lista com todas as pessoas com idade acima da média.'''
+
+censo = list()
+pessoa = dict()
+med = 0
+while True:
+    pessoa['nome'] = str(input('Nome: ')).capitalize()
+    pessoa['sexo'] = 'a'
+    cont = 0
+    while pessoa['sexo'] not in 'MmFf':
+        if cont > 0:
+            print('Erro! Por favor, digite apenas M ou F.')
+        pessoa['sexo'] = str(input('Sexo [M/F]: ')).upper()
+        cont += 1
+    pessoa['idade'] = int(input('Idade: '))
+    med += pessoa['idade']
+    censo.append(pessoa.copy())
+    ag = 'a'
+    cont = 0
+    while ag not in 'SsNn':
+        if cont > 0:
+            print('Erro! Digite apenas S ou N.')
+        ag = str(input('Deseja continuar? [S/N]: '))
+        cont += 1
+    if ag.lower() == 'n':
+        break
+print('-=' * 30)
+print(f'A) Ao todo temos {len(censo)} pessoas cadastradas.')
+media = med / len(censo)
+print(f'B) A média de idade é de {media:.2f}')
+print(f'C) As mulheres cadastradas foram: ', end='')
+for c in range(0, len(censo)):
+    if censo[c]['sexo'] == 'F':
+        print(f'{censo[c]["nome"]} ', end = '')
+print()
+print(f'D) Lista de pessoas que estão acima da média')
+for g in range(0, len(censo)):
+    if censo[g]['idade'] > media:
+        print(f'Nome= {censo[g]["nome"]}; sexo= {censo[g]["sexo"]}; idade= {censo[g]["idade"]};')
+print('<<ENCERRADO>>')
+
+
+
+
+
+
+#print(pessoa)
+#print(censo)
