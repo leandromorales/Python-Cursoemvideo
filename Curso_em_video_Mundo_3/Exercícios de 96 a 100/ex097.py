d98795f12cdcb7491a921be0521e41a30bddb80d
Leandro Morales <leandro.cmorales@gmail.com>
2022-04-13 23:09:15 -0300

    13/04/2022 - Exercício 97.

--- Curso_em_video_Mundo_3/Exercícios de 96 a 100/ex097.py
+++ Curso_em_video_Mundo_3/Exercícios de 96 a 100/ex097.py
@@ -1,3 +1,16 @@
 '''Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e
 mostre uma mensagem com tamanho adaptável.
 '''
+
+def escreva(entrada):
+    guia = len(entrada)
+    print('~' * guia)
+    print(entrada)
+    print('~' * guia)
+
+
+frase1 = str(' Teste ')
+frase2 = str(' Esquistossomose ')
+
+escreva(frase1)
+escreva(frase2)
